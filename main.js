8d512b5c7ebb317066e6707117e1bd8508f3e526
Nazareno Bucciarelli <bnazareno03@gmail.com>
2024-05-23 17:55:54 -0300

    23-05

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ const getCategories = async () => {
     fetch(API_URL + "/products/categories")
         .then((response) => response.json())
         .then((data) => {
-            let dropdownCategories = document.getElementById("dropdown-categories");
             for (let category of data) {
                 let categoryListItem = document.createElement("li");
                 let categoryAnchor = document.createElement("a");
@@ -24,11 +23,13 @@ const getCategories = async () => {
 }
 
 const getProductsByCategoryName = async (categoryName) => {
-    emptyProductsContainer();
+    emptyContainer("product-details-container");
+    emptyContainer("products-container");
     let sectionProducts = document.getElementById("products-container");
     fetch(API_URL + "/products/category/" + categoryName)
         .then((response) => response.json())
         .then((data) => {
+            changeTitle(categoryName.charAt(0).toUpperCase() + categoryName.slice(1));
             for (let product of data) {
                 let productDiv = createProductElement(product);
                 sectionProducts.appendChild(productDiv);
@@ -38,7 +39,9 @@ const getProductsByCategoryName = async (categoryName) => {
 }
 
 const getAllProducts = async () => {
-    emptyProductsContainer();
+    emptyContainer("product-details-container");
+    emptyContainer("products-container");
+    changeTitle("Welcome to Fake Store!")
     let sectionProducts = document.getElementById("products-container");
     fetch('https://fakestoreapi.com/products')
         .then(res => res.json())
@@ -51,12 +54,13 @@ const getAllProducts = async () => {
         .catch((error) => console.error(error));
 }
 
-const emptyProductsContainer = () => {
-    let sectionProducts = document.getElementById("products-container");
-    while (sectionProducts.hasChildNodes()) {
-        sectionProducts.removeChild(sectionProducts.firstChild);
+const emptyContainer = (id) => {
+    let container = document.getElementById(id);
+    while (container.hasChildNodes()) {
+        container.removeChild(container.firstChild);
     }
 }
+
 
 const createProductElement = (product) => {
     let productDiv = document.createElement("div");
@@ -76,35 +80,105 @@ const createProductElement = (product) => {
 
     let productPrice = document.createElement("h4");
     let productPriceText = document.createTextNode("$ " + product.price);
+    productPrice.classList.add("price");
     productPrice.appendChild(productPriceText);
-
-    let productDescription = document.createElement("p");
-    let productDescriptionText = document.createTextNode(product.description);
-    productDescription.classList.add("card-text");
-    productDescription.appendChild(productDescriptionText);
 
     let moreDetailsButton = document.createElement("button");
     moreDetailsButton.classList.add("btn", "btn-primary")
     let moreDetailsButtonText = document.createTextNode("See more details");
     moreDetailsButton.appendChild(moreDetailsButtonText);
+    moreDetailsButton.addEventListener('click', () => viewProductDetailById(product.id, document.getElementById("title").textContent));
 
     productDiv.appendChild(productImage);
     cardBody.append(productTitle);
-    cardBody.append(productDescription);
     cardBody.append(productPrice);
     cardBody.appendChild(moreDetailsButton);
     productDiv.appendChild(cardBody);
 
     return productDiv;
 }
+const changeTitle = (newTitle) => {
+    let title = document.getElementById("title");
+    let titleText = document.createTextNode(newTitle);
+    title.removeChild(title.firstChild);
+    title.appendChild(titleText);
+}
+
+
+const viewProductDetailById = async (id, previousPage) => {
+    fetch(API_URL + "/products/" + id)
+        .then((response) => response.json())
+        .then((product) => {
+            emptyContainer("products-container");
+            changeTitle("Details of " + product.title)
+            let productDetailsContainer = document.getElementById("product-details-container")
+
+            let cardElement = document.createElement("div");
+            cardElement.classList.add("details-card");
+
+            let flexDivElement = document.createElement("div");
+            flexDivElement.classList.add("d-flex", "justify-content-around", "align-items-center", "mb-5");
+
+            let imgElement = document.createElement("img");
+            imgElement.classList.add("card-img-top");
+            imgElement.setAttribute("src", product.image);
+
+            let price = document.createElement("h1");
+            let priceText = document.createTextNode("$ " + product.price);
+            price.appendChild(priceText);
+            price.classList.add("price");
+            let innerDivElement = document.createElement("div");
+            innerDivElement.classList.add("d-flex", "justify-content-center", "flex-column")
+            let addToCartButton = document.createElement("button");
+            addToCartButton.classList.add("btn", "btn-success");
+            let addToCartText = document.createTextNode("Add to cart");
+            addToCartButton.appendChild(addToCartText);
+
+            innerDivElement.appendChild(price);
+            innerDivElement.appendChild(addToCartButton);
+
+            let cardBody = document.createElement("div");
+            cardBody.classList.add("card-body");
+
+            let cardTitle = document.createElement("h5");
+            cardTitle.classList.add("card-title");
+            let cardTitleText = document.createTextNode(product.title);
+            cardTitle.appendChild(cardTitleText);
+
+            let cardText = document.createElement("p");
+            cardText.classList.add("card-text");
+            let cardTextContent = document.createTextNode(product.description);
+            cardText.appendChild(cardTextContent);
+
+            let returnButton = document.createElement("button");
+            let returnText = document.createTextNode("Return back");
+            returnButton.appendChild(returnText);
+            returnButton.classList.add("btn", "btn-secondary", "mt-3");
+            returnButton.addEventListener('click', () => {
+                if (previousPage === "Welcome to Fake Store!") {
+                    getAllProducts();
+                } else {
+                    getProductsByCategoryName(previousPage.toLowerCase())
+                }
+            }
+            )
+
+            cardBody.appendChild(cardTitle);
+            cardBody.appendChild(cardText);
+            flexDivElement.appendChild(imgElement);
+            flexDivElement.appendChild(innerDivElement);
+            cardElement.appendChild(flexDivElement);
+            cardElement.appendChild(cardBody);
+            cardElement.appendChild(returnButton);
+
+            productDetailsContainer.appendChild(cardElement);
+        })
+        .catch((error) => console.error(error));
+}
 
 const main = () => {
     getCategories();
-    let productsContainer =  document.getElementById("products-container");
-    if (!productsContainer.hasChildNodes()) { // TODO
-        console.log("entro")
-        getAllProducts();
-    }
+    getAllProducts();
 }
 
 
